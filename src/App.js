13efaac4dc08be61d54f47c773045e91a2ebf4cc
shimizuren <shimizu@studio-a.co.jp>
2024-12-17 18:34:47 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ function App() {
   return (
 	<div className="App">
 	  <h1>長門有希チャット</h1>
-	  <div className="container comment-box">
-		この銀河を統括する情報統合思念体によって造られた対有機生命体コンタクト用ヒューマノイド・インターフェース、それがわたし。
-	  </div>
-	  <div className="container">
-		<Chat />
+	  <div className="wrapper">
+		<div className="container comment-box">
+		  この銀河を統括する情報統合思念体によって造られた対有機生命体コンタクト用ヒューマノイド・インターフェース、それがわたし。
+		</div>
+		<div className="container">
+		  <Chat />
+		</div>
 	  </div>
 	</div>
   );
