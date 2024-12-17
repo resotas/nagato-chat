3343211e24fe557fca3952cc58ec4f3596568ed7
shimizuren <shimizu@studio-a.co.jp>
2024-12-18 06:21:56 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Chat from "./components/Chat";
 function App() {
   return (
 	<div className="App">
-	  <h1>長門有希チャット</h1>
+	  <h1>Yuki NAGATO.AI</h1>
 	  <div className="wrapper">
 		<div className="container comment-box">
 		  この銀河を統括する情報統合思念体によって造られた対有機生命体コンタクト用ヒューマノイド・インターフェース、それがわたし。
